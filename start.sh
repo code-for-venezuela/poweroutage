9128324794fc45f579fb5ba184d2bfc53bd17d16
Rafael Chacon <rafaelchacon@gmail.com>
2023-04-17 17:48:16 -0400

    fixes wifi connect

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+modprobe i2c-dev
+
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+# Choose a condition for running WiFi Connect according to your use case:
+
+# Is there Internet connectivity via a google ping?
+wget --spider http://google.com 2>&1
+
+# Is there an active WiFi connection?
+iwgetid -r
+
+if [ $? -eq 0 ]; then
+    printf 'Skipping WiFi Connect\n'
+else
+    printf 'Starting WiFi Connect\n'
+    ./wifi-connect
+fi
+
+./poweroutage
